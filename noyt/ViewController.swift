0f9d81b82183a2d93eb23016d04bb69c9fc8f5b1
virenmalhan <virenmalhan.vm@gmail.com>
2018-09-03 12:45:15 +0530

    initial commit

---
+++ noyt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  noyt
+//
+//  Created by Viren Malhan on 03/09/18.
+//  Copyright © 2018 Wecode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
